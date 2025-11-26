4974782a0e348c87c7dfc02f4ad8472c253fdd97
ijack-22 <bmccess@gmail.com>
2025-11-26 06:25:57 +0000

    Add unit tests for FileEncryptor class

---
+++ tests/tests/tests/test_encryptor.py
@@ -0,0 +1,27 @@
+import unittest
+import os
+import tempfile
+from src.encryptor import FileEncryptor
+
+class TestFileEncryptor(unittest.TestCase):
+    
+    def setUp(self):
+        self.encryptor = FileEncryptor()
+        self.test_password = "test_password_123"
+        self.key, self.salt = self.encryptor.generate_key_from_password(self.test_password)
+        self.encryptor.initialize_encryptor(self.key)
+    
+    def test_key_generation(self):
+        key, salt = self.encryptor.generate_key_from_password("test")
+        self.assertIsNotNone(key)
+        self.assertIsNotNone(salt)
+        self.assertEqual(len(salt), 16)
+    
+    def test_string_encryption_decryption(self):
+        original_text = "This is a secret message!"
+        encrypted = self.encryptor.encrypt_string(original_text)
+        decrypted = self.encryptor.decrypt_string(encrypted)
+        self.assertEqual(original_text, decrypted)
+
+if __name__ == '__main__':
+    unittest.main()
